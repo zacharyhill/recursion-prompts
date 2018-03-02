75a6d202183b98c3dbb379f309fe0c25f1b71e8b
Zachary Hill <zach@zacharyhill.xyz>
2018-03-02 14:46:32 -0600

    Implement powerOfTwo and exponent functions

--- src/recursion.js
+++ src/recursion.js
@@ -93,6 +93,13 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  } else if (exp > 0) {
+    return base * exponent(base, exp - 1);
+  } else if (exp < 0) {
+    return 1 / exponent(base, exp * -1);
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -100,6 +107,13 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  } else if (n < 2) {
+    return false;
+  } else {
+    return powerOfTwo(n / 2);
+  }
 };
 
 // 9. Write a function that reverses a string.
