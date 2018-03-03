ec4c96b9b3138e692c136c3c895c9398e0af3ae1
Zachary Hill <zach@zacharyhill.xyz>
2018-03-02 21:05:41 -0800

    Implement modulo function

--- src/recursion.js
+++ src/recursion.js
@@ -148,6 +148,28 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x < 0 && y < 0) {
+    var positiveX = x - x - x;
+    var positiveY = y - y - y;
+    var positiveRemainder = modulo(positiveX, positiveY);
+    return positiveRemainder - positiveRemainder - positiveRemainder;
+  } else if (x < 0) {
+    var positiveX = x - x - x;
+    var positiveRemainder = modulo(positiveX, y);
+    return positiveRemainder - positiveRemainder - positiveRemainder;
+  } else if (y < 0) {
+    var positiveY = y - y - y;
+    var positiveRemainder = modulo(x, positiveY);
+    return positiveRemainder - positiveRemainder - positiveRemainder;
+  } else if (x === 0 && y === 0) {
+    return NaN;
+  } else if (x === 0) {
+    return 0;
+  } else if ((x - y) < 0) {
+    return x;
+  } else {
+    return modulo(x - y, y);
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
