95f4e410f173166cf607cf040642fe6c0a1d9352
Zachary Hill <zach@zacharyhill.xyz>
2018-03-03 09:42:05 -0800

    Implement build an array with a given length function

--- src/recursion.js
+++ src/recursion.js
@@ -270,6 +270,11 @@ var reverseArr = function(array) {
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  if (!length) {
+    return [];
+  } else {
+    return [value].concat(buildList(value, length - 1));
+  }
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
